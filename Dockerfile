ee5f1fe35e7fc0818a7528d0352504592020f5ef
Lara Maia <dev@lara.click>
2016-10-31 05:59:12 -0200

    Add portage config

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ MAINTAINER Lara Maia - dev@lara.click
 LABEL Description="Gentoo with portage, git, repoman, systemd"
 
 ADD install_portage.sh /
-ADD set_use_flags.sh /
+ADD set_config.sh /
 ADD update_system.sh /
 ADD install_extra_packages.sh /
 
 RUN /install_portage.sh
-RUN /set_use_flags.sh
+RUN /set_config.sh
 RUN /update_system.sh
 RUN /install_extra_packages.sh
---
+++ set_config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Lara Maia <dev@lara.click> 2016
+
+mkdir -p /etc/portage
+
+USE="systemd -consolekit"
+ACCEPT_KEYWORDS="**"
+ACCEPT_LICENSE="*"
+PYTHON_SINGLE_TARGET="python3_4"
+PYTHON_TARGETS="python3_4 python2_7"
+RUBY_TARGETS="ruby23"
+
+echo "USE=\"\$USE $USE\"" >> /etc/portage/make.conf
+echo "ACCEPT_KEYWORDS=\"$ACCEPT_KEYWORDS\"" >> /etc/portage/make.conf
+echo "ACCEPT_LICENSE=\"$ACCEPT_LICENSE\"" >> /etc/portage/make.conf
+echo "PYTHON_SINGLE_TARGET=\"$PYTHON_SINGLE_TARGET\"" >> /etc/portage/make.conf
+echo "PYTHON_TARGETS=\"$PYTHON_TARGETS\"" >> /etc/portage/make.conf
+echo "RUBY_TARGETS=\"$RUBY_TARGETS\"" >> /etc/portage/make.conf
+
+unset USE
+unset ACCEPT_KEYWORDS
+unset ACCEPT_LICENSE
+unset PYTHON_SINGLE_TARGET
+unset PYTHON_TARGETS
+unset RUBY_TARGETS
